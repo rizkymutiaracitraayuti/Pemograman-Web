bef99e4c07c823d20b2eb80148c46762346b5bae
rizkymutiaracitraayuti <82300311+rizkymutiaracitraayuti@users.noreply.github.com>
2022-04-11 13:51:42 +0700

    Add files via upload

---
+++ array/no1array.php
@@ -0,0 +1,160 @@
+<?php
+//array data mahasiswa
+$mhs = [
+    ["nrp"=>"001", "nama"=>"Hoirotun Nafiah", "ipk"=>3.20, "wrambut"=>"Hitam", "brambut"=>"Ikal", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"002", "nama"=>"Nurun Dece R", "ipk"=>3.10, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"003", "nama"=>"Shelma Imara", "ipk"=>3.40, "wrambut"=>"Coklat", "brambut"=>"ikal", "bbadan"=>"60", "tbadan"=>"170", "status"=>"Cuti"],
+    ["nrp"=>"004", "nama"=>"Rizky Mutiara Citra", "ipk"=>3.15, "wrambut"=>"Coklat", "brambut"=>"Lurus", "bbadan"=>"55", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"005", "nama"=>"Fadiyatul", "ipk"=>3.40, "wrambut"=>"Hitam", "brambut"=>"Keriting", "bbadan"=>"60", "tbadan"=>"170", "status"=>"Aktif"],
+    ["nrp"=>"006", "nama"=>"Rara kirana", "ipk"=>3.20, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"007", "nama"=>"RIzqi Husna", "ipk"=>3.00, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"45", "tbadan"=>"160", "status"=>"Cuti"],
+    ["nrp"=>"008", "nama"=>"Zaskhia Artina", "ipk"=>3.55, "wrambut"=>"Coklat", "brambut"=>"Lurus", "bbadan"=>"70", "tbadan"=>"180", "status"=>"Cuti"],
+    ["nrp"=>"009", "nama"=>"Baga Arhinza", "ipk"=>3.15, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"010", "nama"=>"Rica Elfira", "ipk"=>3.10, "wrambut"=>"Hitam", "brambut"=>"Keriting", "bbadan"=>"50", "tbadan"=>"165", "status"=>"Aktif"],
+    ["nrp"=>"011", "nama"=>"Widya Titah", "ipk"=>3.30, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"80", "tbadan"=>"175", "status"=>"Aktif"],
+    ["nrp"=>"012", "nama"=>"Ramdhan B", "ipk"=>3.18, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"013", "nama"=>"Ahmad Faza", "ipk"=>3.55, "wrambut"=>"Coklat", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Cuti"],
+    ["nrp"=>"014", "nama"=>"Amalia", "ipk"=>3.15, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"015", "nama"=>"Nizar", "ipk"=>3.08, "wrambut"=>"Hitam", "brambut"=>"Keriting", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"016", "nama"=>"Orin", "ipk"=>3.30, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"80", "tbadan"=>"175", "status"=>"Aktif"],
+    ["nrp"=>"017", "nama"=>"Vina Maulidya", "ipk"=>3.80, "wrambut"=>"Coklat", "brambut"=>"Keriting", "bbadan"=>"55", "tbadan"=>"165", "status"=>"Cuti"],
+    ["nrp"=>"018", "nama"=>"Mirna Milasari", "ipk"=>3.75, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"45", "tbadan"=>"150", "status"=>"Aktif"],
+    ["nrp"=>"019", "nama"=>"Iqbal Haqiqi", "ipk"=>3.10, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"020", "nama"=>"Iksanul", "ipk"=>3.08, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"021", "nama"=>"Rizal Aditya", "ipk"=>3.30, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"80", "tbadan"=>"175", "status"=>"Aktif"],
+    ["nrp"=>"022", "nama"=>"Alda Agustin", "ipk"=>3.18, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"023", "nama"=>"Irfan Aliftya", "ipk"=>3.55, "wrambut"=>"Coklat", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Cuti"],
+    ["nrp"=>"024", "nama"=>"Okky Anugerah", "ipk"=>3.15, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"025", "nama"=>"Ananda Irvan", "ipk"=>3.08, "wrambut"=>"Hitam", "brambut"=>"Keriting", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"026", "nama"=>"Chayani Suci", "ipk"=>3.30, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"80", "tbadan"=>"175", "status"=>"Aktif"],
+    ["nrp"=>"027", "nama"=>"Nomar samaa", "ipk"=>3.80, "wrambut"=>"Coklat", "brambut"=>"Keriting", "bbadan"=>"55", "tbadan"=>"165", "status"=>"Cuti"],
+    ["nrp"=>"028", "nama"=>"Irma Mustafidah", "ipk"=>3.75, "wrambut"=>"Coklat", "brambut"=>"Ikal", "bbadan"=>"45", "tbadan"=>"150", "status"=>"Aktif"],
+    ["nrp"=>"029", "nama"=>"Fauzi Setiawan", "ipk"=>3.10, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"],
+    ["nrp"=>"030", "nama"=>"Aulya Firly", "ipk"=>3.08, "wrambut"=>"Hitam", "brambut"=>"Lurus", "bbadan"=>"50", "tbadan"=>"160", "status"=>"Aktif"]
+];
+?>
+<h1>DATA MAHASISWA PRODI PTI UNESA</h1>
+<br>
+<table border="1">
+    <thead>
+        <tr>
+            <th>NRP</th>
+            <th>NAMA</th>
+            <th>IPK</th>
+            <th>WARNA RAMBUT</th>
+            <th>BENTUK RAMBUT</th>
+            <th>BERAT BADAN</th>
+            <th>TINGGI BADAN</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($mhs as $data): ?>
+        <tr>
+            <td><?=$data["nrp"]?></td>
+            <td><?php echo $data["nama"]?></td>
+            <td><?php echo $data["ipk"]?></td>
+            <td><?php echo $data["wrambut"]?></td>
+            <td><?php echo $data["brambut"]?></td>
+            <td><?php echo $data["bbadan"]?></td>
+            <td><?php echo $data["tbadan"]?></td>
+            <td><?php echo $data["status"]?></td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+
+<?php
+//if else
+echo "<br>";
+echo "Hasil Pencarian Mahasiswa : "."<br>";
+echo "<br>"."------------------IF ELSE--------------------"."<br>";
+echo "<br>";
+for ($i=0;$i < count($mhs); $i++){
+    if($mhs[$i]["nrp"] >= 001 && $mhs[$i]["nrp"] <=005){
+       if ($mhs[$i]["ipk"] > 3.00 && $mhs[$i]["ipk"] < 3.25){
+           if ($mhs[$i]["wrambut"] == "Hitam" && $mhs[$i]["brambut"] == "Lurus"){
+               if ($mhs[$i]["bbadan"] == 50 && $mhs[$i]["tbadan"] == 160){
+                   if ($mhs[$i]["status"] !== "Cuti"){
+                       echo "Nama  :" .$mhs[$i]["nama"]."<br>";
+                       echo "NRP   :" .$mhs[$i]["nrp"]."<br>";
+                       echo "<br>";
+                   }
+               }
+           }
+       }
+   }
+}
+
+//switch case
+echo "<hr>"."<br>";
+echo "------------------SWITCH CASE--------------------"."<br>";
+echo "<br>";
+for($i=5 ; $i < 10; $i++){
+    switch($mhs){
+        case(($mhs[$i]["ipk"] > 3.00 && $mhs[$i]["ipk"] < 3.25) && ($mhs[$i]["wrambut"] == "Hitam" && $mhs[$i]["brambut"] == "Lurus") && ($mhs[$i]["bbadan"] == 50 && $mhs[$i]["tbadan"] == 160) && ($mhs[$i]["status"] !== "Cuti")) :
+            echo "Nama  :" .$mhs[$i]["nama"]."<br>";
+            echo "NRP   :" .$mhs[$i]["nrp"]."<br>";
+            echo "<br>";
+            break;
+    }
+}
+
+//do while
+echo "<hr>"."<br>";
+echo "------------------DO WHILE--------------------"."<br>";
+echo "<br>";
+do{
+    if($mhs[$i]["nrp"] >=11 && $mhs[$i]["nrp"] <=15){
+        if ($mhs[$i]["ipk"] > 3.00 && $mhs[$i]["ipk"] < 3.25){
+            if ($mhs[$i]["wrambut"] == "Hitam" && $mhs[$i]["brambut"] == "Lurus"){
+                if ($mhs[$i]["bbadan"] == 50 && $mhs[$i]["tbadan"] == 160){
+                    if ($mhs[$i]["status"] !== "Cuti"){
+                        echo "Nama  :" .$mhs[$i]["nama"]."<br>";
+                        echo "NRP   :" .$mhs[$i]["nrp"]."<br>";
+                        echo "<br>";
+                    }
+                }
+            }
+        }
+    }
+    $i++;
+}while ($i<sizeof($mhs));
+
+//for loop
+echo "<hr>"."<br>";
+echo "------------------FOR LOOP--------------------"."<br>";
+echo "<br>";
+for ($i=15; $i < 20; $i++){
+    if ($mhs[$i]["ipk"] > 3.00 && $mhs[$i]["ipk"] < 3.25){
+        if ($mhs[$i]["wrambut"] == "Hitam" && $mhs[$i]["brambut"] == "Lurus"){
+            if ($mhs[$i]["bbadan"] == 50 && $mhs[$i]["tbadan"] == 160){
+                if ($mhs[$i]["status"] !== "Cuti"){
+                    echo "Nama  :" .$mhs[$i]["nama"]."<br>";
+                    echo "NRP   :" .$mhs[$i]["nrp"]."<br>";
+                    echo "<br>";
+                }
+            }
+        }
+    }
+}
+
+//while
+echo "<hr>"."<br>";
+echo "------------------WHILE--------------------"."<br>";
+echo "<br>";
+while($i<sizeof($mhs)){
+    if($mhs[$i]["nrp"] >=21 && $mhs[$i]["nrp"] <=30){
+       if ($mhs[$i]["ipk"] > 3.00 && $mhs[$i]["ipk"] < 3.25){
+           if ($mhs[$i]["wrambut"] == "Hitam" && $mhs[$i]["brambut"] == "Lurus"){
+               if ($mhs[$i]["bbadan"] == 50 && $mhs[$i]["tbadan"] == 160){
+                   if ($mhs[$i]["status"] !== "Cuti"){
+                       echo "Nama  :" .$mhs[$i]["nama"]."<br>";
+                       echo "NRP   :" .$mhs[$i]["nrp"]."<br>";
+                       echo "<br>";
+                   }
+               }
+           }
+       }
+   }$i++;
+};
